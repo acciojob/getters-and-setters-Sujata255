1f50ae34e8b99f1ef2310a8642c7e146ef77c8b7
Sujata255 <das19sujata@gmail.com>
2024-10-11 22:55:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-//complete this code
-class Person {}
-
-class Student extends Person {}
-
-class Teacher extends Person {}
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
-
 
 class Person{
 	constructor(name,age){
